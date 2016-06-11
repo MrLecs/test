e00eddadef3a85751a6e5a4ca3123e7ddbf2fdbd
Sergey <mastedm@gmail.com>
2016-06-12 01:37:42 +0300

    add comments for code

--- app/controllers/master/testings_controller.rb
+++ app/controllers/master/testings_controller.rb
@@ -1,32 +1,87 @@
+#
+# Контроллер выведен из класса MasterController, в котором определяются 
+# специфичные для пространста Master вещи. Например, авторизация пользователя
+# через Basic Authentication и laout master.
+#
 class Master::TestingsController < MasterController
+  
+  #
+  # Перед выполнением методов, указанных в массиве only, выполняется метод
+  # set_testing, который устанавливает переменную @testing
+  #
   before_action :set_testing, only: [:show, :edit, :update, :destroy, :questions]
 
+  #
+  # Выводит все объекты Testing, которые есть в базе
+  #
   # GET /testings
   # GET /testings.json
   def index
+    #
+    # Запрос к модели Testing. SQL-эквивалент: 'SELECT * FROM testings'
+    #
     @testings = Testing.all
   end
 
+
+  #
+  # Отображает выбранные объект @testing. Метод пустой, так как ранее через
+  # before_action необходимый объект уже был выбран
+  #
   # GET /testings/1
   # GET /testings/1.json
   def show
   end
 
+  #
+  # Создание нового объекта Testing.
+  #
   # GET /testings/new
   def new
+    # 
+    # Создаем пустой объект, который передается во views
+    #
     @testing = Testing.new
   end
 
+  #
+  # Редактирование объекта Testing.
+  #
   # GET /testings/1/edit
   def edit
   end
 
+  #
+  # Сохранение нового объекта в базе данных
+  #
   # POST /testings
   # POST /testings.json
   def create
+    #
+    # Создаем объект на основании переданных из html-формы параметров. Для этого
+    # вызывается метод testing_params
+    #
     @testing = Testing.new(testing_params)
 
+    #
+    # В зависимости от формата запроса - html или json - определяется отрисовка
+    #
     respond_to do |format|
+      #
+      # Сохраняем объект. 
+      #
+      # В случае если сохранение прошло успешно, метод save
+      # вернет true и мы делаем перенапралвение на страницу со списком
+      # тестирований.
+      #
+      # Если save возвращает false, это значит, что объект в базу данных не 
+      # сохранился. Отрисовывается страница для создания (view для метода new),
+      # при этом будет выведено сообщение с описанием возникшей ошибки.
+      #
+      # Объект может быть не сохранен в базе по причине того, что поля не
+      # проходят условия валидации. Например, для модели Testing у нас
+      # определено условие, что timeout строго больше 0.
+      #
       if @testing.save
         format.html { redirect_to master_testings_url, notice: 'Testing was successfully created.' }
         format.json { render :show, status: :created, location: @testing }
@@ -37,10 +92,21 @@ class Master::TestingsController < MasterController
     end
   end
 
+  #
+  # Обновление объекта. 
+  #
   # PATCH/PUT /testings/1
   # PATCH/PUT /testings/1.json
   def update
+    #
+    # В зависимости от формата запроса - html или json - определяется отрисовка
+    #
     respond_to do |format|
+      #
+      # Для объекта @testing проставляются поля, переданные из html формы.
+      # Аналогично как при создании объекта, выполняется проверка условий 
+      # валидации
+      #
       if @testing.update(testing_params)
         format.html { redirect_to master_testings_url, notice: 'Testing was successfully updated.' }
         format.json { render :show, status: :ok, location: @testing }
@@ -51,26 +117,47 @@ class Master::TestingsController < MasterController
     end
   end
 
+  # 
+  # Удаление объекта
+  #
   # DELETE /testings/1
   # DELETE /testings/1.json
   def destroy
+    # 
+    # Удаление объекта
+    #
     @testing.destroy
+
+    #
+    # Отрисовка в зависимости от формата запроса
+    #
     respond_to do |format|
       format.html { redirect_to master_testings_url, notice: 'Testing was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
+  #
+  # Метод отвечате за отображение списка вопросов для теста. Метод пустой, так
+  # как необходимый объект был выбран чере before_action, а основной код
+  # отрисовки находится во views
+  #
   def questions
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+    #
+    # Устанавливаем объект @testing по параметру id в запросе
+    #
     def set_testing
       @testing = Testing.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    #
+    # Определяет 'белый список' параметров, которые приходят от html-формы и 
+    # могут быть использованы в массовом назначении для моделей (как, например,
+    # при создании или редактировании)
+    #
     def testing_params
       params.require(:testing).permit(:title, :description, :timeout, :random_questions, :random_answers)
     end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,38 +1,84 @@
 class StudentsController < ApplicationController
+  
+  #
+  # Перед выполнением любого метода котроллера, проверяем аутентификацию
+  # пользователя. 
+  #
   before_action :authenticate_student!
+  
+  #
+  # Перед выполнением методов, указанных в массиве only, вызываем set_question
+  #
   before_action :set_question, only: [:question, :answer, :skip_question]
+  
+  #
+  # Перед выполнением методов, указанных в массиве only, вызываем set_testing
+  #
   before_action :set_testing,  only: [:question, :answer, :skip_question, :finish]
   
   layout 'students'
   
+  #
+  # Выводим список всех тестов
+  #
   def index
     @testings = Testing.all
   end
   
+  #
+  # Стартуем выполнение теста
+  #
   def start
+    #
+    # Выбираем нужный тест
+    #
     @testing    = Testing.find(params[:testing_id].to_i)
+    
+    #
+    # Создаем объект класса TestSuite для авторизованного студента и выбранного
+    # теста
+    #
     @test_suite = TestSuite.start(student: current_student, testing: @testing)
 
+    #
+    # Инициализируем Cookies
+    #
     init_cookies
 
+    #
+    # Перенаправляем пользователя на первый вопрос теста
+    #
     redirect_to testing_path(:question)
   end
 
+  #
+  # Отрисовка вопроса. Сам объект вопроса уже выбран благодаря before_action 
+  #
   def question
   end
   
+  #
+  # Отмечаем ответ студента на вопрос
+  #
   def answer
     @test_suite.answer(question: @question, answers: Answer.find(params['answers'])) rescue nil
 
     next_question
   end
 
+  #
+  # Студент пропускает вопрос. Отмечаем этот факт и переходим к следующему 
+  # вопросу
+  #
   def skip_question
     @test_suite.skip_question(question: @question)
 
     next_question
   end
   
+  #
+  # Окончание теста
+  #
   def finish
     @test_suite.finish
     
@@ -41,17 +87,29 @@ class StudentsController < ApplicationController
   
   private
   
+  #
+  # Устанавливаем вопрос на основании Cookies. В Cookies у нас хранится
+  # порядковый номер вопроса в построенном списке вопросов. По нему определяется
+  # вопрос и достается из базы данных в переменную @question.
+  #
   def set_question
     @idx       = cookies['idx'].to_i
     @questions = JSON.parse(cookies['questions'])
     @question  = Question.find(@questions[@idx])
   end
 
+  #
+  # Установка переменных @testing и @test_suite
+  #
   def set_testing
     @testing    = Testing.find(cookies['tid']) rescue nil
     @test_suite = TestSuite.find(cookies['tsid']) rescue nil
   end
 
+  #
+  # Переход к следующему вопросу. В Cookies меняется порядковый номер вопроса
+  # (+1). Если вопросы закончились, то редирект на окончание тестирования
+  #
   def next_question
     if @idx + 1 < @questions.size
       cookies['idx'] = @idx + 1
@@ -80,6 +138,12 @@ class StudentsController < ApplicationController
     cookies['tid']  = nil
   end
   
+  #
+  # Построение списка вопросов. Для начала выбираются те вопросы, которые НЕ
+  # являются дополнительными кому-либо. Перемешиваем их, если выбрана
+  # соответствующая галочка в настройках теста. Далее по списку вопросов 
+  # выгружаются дополнительные.
+  #
   def questions_list
     questions = @testing.questions.where(parent_question_id: nil).to_a
     questions.shuffle! if @testing.random_questions?
